1f43f86e4ceddc774b8586853f27d5524b4ac8b7
AntonGerasimov <antoshkasg@gmail.com>
2016-10-05 11:46:51 +0300

    UPD at the end of the lesson 05.10.2016

--- root/func.c
+++ root/func.c
@@ -8,11 +8,26 @@ void a(){
 	c->Divide(3,2);
 	c->cd(1);
 	f->Draw();
-	TH1F *hist1 = new TH1F("hist1", "hist", 100, 0., 20.);
+	TH1F *hist1 = new TH1F("hist1", "hist2", 100, 0., 20.);
 	for (int i=0; i<1000; i++){
 		hist1->Fill(f->GetRandom());
 	}
 	c->cd(2);
-	f->Draw();
-//hist1->Draw();
+	hist1->Draw();
+	TH1F *hist2 = new TH1F("hist2", "hist2", 100, 0., 20.);
+	for (int i=0; i<1000; i++){
+		hist2->Fill(f->GetRandom());
+	}
+	c->cd(3);
+	hist2->Draw();
+	TH1F *hist3 = new TH1F("hist3", "hist3", 100, 0., 20.);
+	hist3->Add(hist1, hist2, 2, 0.5);
+	c->cd(4);
+	hist3->Draw();
+//	TH1F *hist4 = new TH1F("hist4", "hist4", 100, 0., 20.);
+//	hist4->sumw2();
+	
+//	c->cd(5);
+//	hist4->Draw();
+//	c->Update();
 }
